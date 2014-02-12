db9fe727469b4aa428d701c2fb6a3b8eb31e86ee
RamonDeOliveiraSilva <ramondeosilva@gmaiil.com>
2014-02-12 08:06:55 -0200

    arquivos novos

---
+++ Versao/Projeto/src/cp/com/Equipe.java
@@ -0,0 +1,45 @@
+package cp.com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(detachable = "true")
+public class Equipe {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private List<Jogador> jogadores = new ArrayList<Jogador>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Jogador> getJogadores() {
+		return jogadores;
+	}
+
+	public void setJogadores(List<Jogador> jogadores) {
+		this.jogadores = jogadores;
+	}
+
+}
